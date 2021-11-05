341ef111149f34dbd0ac2d2db723e0e59df1a9e1
Darius Mann <dsmanyp@gmail.com>
2021-11-05 09:20:52 +0200

    install and configure vue gtag (google analytics)

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import DatetimePicker from 'vuetify-datetime-picker'
 import i18n from '@/plugins/i18n'
 import CountryFlag from 'vue-country-flag'
 import 'vue-phone-number-input/nuxt/plugin'
+import VueGtag from 'vue-gtag'
 
 ApiService.init(process.env.VUE_APP_BACKEND_API_PROXY_URL)
 
@@ -19,10 +20,15 @@ Vue.component('country-flag', CountryFlag)
 
 Vue.use(DatetimePicker)
 
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GOOGLE_ANALYTICS_TRACKING_CODE }
+}, router)
+
 new Vue({
   router,
   store,
   vuetify,
   i18n,
+  gtag: VueGtag,
   render: h => h(App)
 }).$mount('#app')
