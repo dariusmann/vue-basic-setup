0ca43abbc42ccd16975a399baf8ed6ebe2eb5214
Darius Mann <dsmanyp@gmail.com>
2021-11-06 11:00:27 +0200

    starttime calendar 6:00 am

--- src/components/Game/Finder/GameCalendar.vue
+++ src/components/Game/Finder/GameCalendar.vue
@@ -3,7 +3,7 @@
     <v-calendar
       :type="calendarType"
       :events="getEvents()"
-      :first-time="'09:00'"
+      :first-time="'06:00'"
       @click:event="showEvent"
     />
     <CalendarEventCard
